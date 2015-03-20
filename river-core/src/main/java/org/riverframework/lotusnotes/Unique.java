c3c2fcae6b6c811779b41b990c89768fadd06b7b
Mario Sotil <mario.sotil@gmail.com>
2015-03-20 06:49:46 -0400

    Revert "Revert "Changed lotus.domino.* for org.openntf.domino.*""
    This reverts commit ce4354116dfa74db10d7c7c63a687437d3d5a0f9.

--- river-core/src/main/java/org/riverframework/lotusnotes/Unique.java
+++
@@ -1,9 +0,0 @@
-package org.riverframework.lotusnotes;
-
-public interface Unique extends org.riverframework.Unique {
-	@Override
-	public org.riverframework.lotusnotes.Document setId(String id);
-
-	@Override
-	public org.riverframework.lotusnotes.Document generateId();
-}
---
+++ river-demo/src/main/java/org/riverframework/domino/demo/PeopleAddressBook.java
@@ -0,0 +1,76 @@
+package org.riverframework.domino.demo;
+
+import org.openntf.domino.Document;
+import lotus.domino.NotesThread;
+
+import org.riverframework.domino.*;
+
+/*
+ * To run this demo, you will need:
+ * 
+ * - IBM Notes installed in your computer
+ * - A NSF database called PeopleAddressBook.nsf 
+ * 
+ * This database must have this design elements:
+ * 
+ * - A form called "People | fo_ap_people"
+ * - A view called "People Index | vi_ap_people_index"
+ * 
+ * The form must have these fields:
+ * 
+ * - ca_pe_name
+ * - ca_pe_address
+ * - ca_pe_email
+ * - ca_pe_phone
+ * 
+ * The view must have this one column:
+ * 
+ * - "Name", with values from the field ca_pe_name, ordered in ascending form
+ * 
+ * The view must have this selection formula:
+ * 
+ * - SELECT Form = "fo_ap_people"
+ * 
+ */
+
+public class PeopleAddressBook {
+	private static final String filepath = "PeopleAddressBook.nsf";
+
+	static class People extends DefaultDocument implements Unique {
+
+		protected People(Database d, Document doc) {
+			super(d, doc);
+		}
+
+		public static String getIndexName() {
+			return "vi_ap_people_index";
+		}
+
+		public String getId() {
+			return getFieldAsString("ca_pe_name");
+		}
+
+		public org.riverframework.domino.Document generateId() {
+			// Do nothing
+			return this;
+		}
+
+		public org.riverframework.domino.Document setId(String arg0) {
+			setField("ca_pe_name", arg0);
+			return this;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		NotesThread.sinitThread();
+
+		Session session = DefaultSession.getInstance().open("passw0rd");
+		System.out.println("User=" + session.getNotesSession().getCommonUserName());
+		Database database = session.getDatabase(DefaultDatabase.class, "", filepath);
+		System.out.println("Database=" + database.getName());
+
+		NotesThread.stermThread();
+	}
+
+}

c71ba3b2bdb6334420807c090709111dd74a3faf
Mario Sotil <mario.sotil@gmail.com>
2015-03-26 06:49:59 -0400

    Version 0.2.5

--- river-core/src/main/java/org/riverframework/domino/Session.java
+++
@@ -1,9 +0,0 @@
-package org.riverframework.domino;
-
-public interface Session extends org.riverframework.Session {
-	public lotus.domino.DateTime Java2NotesTime(java.util.Date d);
-
-	@Override
-	public org.riverframework.domino.Session open(String... parameters);
-
-}

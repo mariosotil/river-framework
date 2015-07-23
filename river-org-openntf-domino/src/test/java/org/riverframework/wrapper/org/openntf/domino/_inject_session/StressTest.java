00f3f5db6263abf7a888198c4ce70ce57b65aeb8
Mario Sotil <mario.sotil@gmail.com>
2015-07-23 02:21:46 -0400

    Working on 0.2.9
    - Fixing pom.xml
    - Updating tests for org-openntf-domino

--- river-org-openntf-domino/src/test/java/org/riverframework/wrapper/org/openntf/domino/_inject_session/StressTest.java
+++ river-org-openntf-domino/src/test/java/org/riverframework/wrapper/org/openntf/domino/_inject_session/StressTest.java
@@ -9,7 +9,7 @@ import java.util.logging.Logger;
 
 import org.riverframework.River;
 
-public class NativeReferenceCollectorTest extends org.riverframework.wrapper.org.openntf.domino.AbstractStressTest {
+public class StressTest extends org.riverframework.wrapper.AbstractStressTest {
 	protected static final Logger log = River.LOG_WRAPPER_ORG_OPENNTF_DOMINO;
 	
 	@BeforeClass
--- river-org-openntf-domino/src/test/java/org/riverframework/wrapper/org/openntf/domino/_local/StressTest.java
+++ river-org-openntf-domino/src/test/java/org/riverframework/wrapper/org/openntf/domino/_local/StressTest.java
@@ -8,7 +8,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.riverframework.River;
 
-public class StressTest extends org.riverframework.wrapper.org.openntf.domino.AbstractStressTest {
+public class StressTest extends org.riverframework.wrapper.AbstractStressTest {
 	protected static final Logger log = River.LOG_WRAPPER_ORG_OPENNTF_DOMINO;
 
 	@BeforeClass
